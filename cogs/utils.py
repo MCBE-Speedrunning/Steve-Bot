ba4272f2a15376b3df4880c97a7bfa7f445eb0c4
AnInternetTroll <lucafulger@gmail.com>
2020-06-28 22:19:06 +0100

    Cooldown of 60 sec to @someone

--- cogs/utils.py
+++ cogs/utils.py
@@ -220,6 +220,7 @@ class Utils(commands.Cog):
 			#return
 			await ctx.send(f"{ctx.message.author.display_name}, you have to wait {round(error.retry_after, 2)} seconds before using this again.")
 
+	@commands.cooldown(1, 60, commands.BucketType.guild)
 	@commands.command()
 	async def someone(self, ctx):
 		if ctx.channel.id != int(self.bot.config[str(ctx.message.guild.id)]["fair_channel"]):
