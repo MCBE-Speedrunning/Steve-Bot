f7b54808be04d550283de15faf31a53f364e5de6
ziro <palembani@gmail.com>
2020-08-05 12:37:58 +0700

    + Remove logging temporarily

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -7,7 +7,6 @@ class Welcome(commands.Cog):
     # Welcome message + set roles when new member joined
     def __init__(self, bot):
         self.bot = bot
-        self.logger = logging.getLogger('discord')
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
@@ -15,7 +14,6 @@ class Welcome(commands.Cog):
         welcome_channel = self.bot.get_channel(740051039499059271)
         member_role = discord.utils.get(member.guild.roles, name="Member")
         await member.add_roles(member_role)
-        self.logger.info(f'New member: {member.name} (ID: {member.id})'
         await welcome_channel.send(f"Welcome {member.mention}, to {server.name}! <:PogChamp:740102448047194152>")
 
     @commands.command(aliases=['test'])
