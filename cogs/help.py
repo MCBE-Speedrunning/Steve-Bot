f797397950a1757a978962e912edae1a7263d3cb
ziro <palembani@gmail.com>
2020-08-06 13:48:02 +0700

    + Added listcommands to help.py (from mcbeDiscordBot) >Fixed the trailing comma from listcommands

--- cogs/help.py
+++ cogs/help.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 import discord
 import asyncio
+import json
 
 class Help(commands.Cog):
     def __init__(self, bot):
@@ -26,6 +27,21 @@ class Help(commands.Cog):
             _cmd = " | ".join([str(cmd),*cmd.aliases])
             embed.add_field(name=f"{_cmd}", value=f"{_desc}", inline=False)
 
+        await ctx.send(embed=embed)
+
+    @commands.command(aliases=['customcommands', 'ccmds'])
+    async def listcommands(self, ctx):
+        """List all custom commands"""
+        embed = discord.Embed(
+                    title = "Help",
+                    colour = discord.Colour.gold()
+                )
+        with open('custom_commands.json', 'r') as f:
+            commands = json.load(f)
+            ccmds = ", ".join([*commands])
+            # await ctx.send(f"```List of custom commands: \n{ccmds}```")
+            # output += f'{ccmds}```'
+        embed.add_field(name="Custom Commands", value=f"{ccmds}", inline=False)
         await ctx.send(embed=embed)
 
 def setup(bot):
