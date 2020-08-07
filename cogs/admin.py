8d649f025d4ea1ab7236c87028925289f791b43c
Thomas Voss <57815710+Mango0x45@users.noreply.github.com>
2020-08-07 02:37:09 +0200

    Update admin.py

--- cogs/admin.py
+++ cogs/admin.py
@@ -316,7 +316,7 @@ class Admin(commands.Cog):
         @commands.command()
         @commands.check(is_botmaster)
         async def give_role(self, ctx, role_id):
-            await ctx.author.add_roles(role_id)
+                await ctx.author.add_roles(role_id)
 
 
 def setup(bot):
